c9a3181276c373696bbf059256e14e09d8d4be79
Ferwardo <r0741339@student.thomasmore.be>
2020-10-28 15:03:42 +0100

    Initial project

---
+++ src/main/java/com/dezoo/edge/DeZooEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dezoo.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeZooEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeZooEdgeServiceApplication.class, args);
+    }
+
+}
