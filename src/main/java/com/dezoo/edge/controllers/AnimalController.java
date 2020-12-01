3c0621a38c202538e29fcd385fe8559d49bf6b75
Ferwardo <r0741339@student.thomasmore.be>
2020-12-01 13:47:41 +0100

    added restTemplate to controllers

--- src/main/java/com/dezoo/edge/controllers/AnimalController.java
+++ src/main/java/com/dezoo/edge/controllers/AnimalController.java
@@ -1,9 +1,13 @@
 package com.dezoo.edge.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("animals")
 public class AnimalController {
+	@Autowired
+	private RestTemplate restTemplate;
 }
