a0b04ecfb7fd2c29f6d86afe2f3dd137965d6bea
Ferwardo <r0741339@student.thomasmore.be>
2020-12-01 14:07:36 +0100

    added first model, redid controllers into one ZooController

--- src/main/java/com/dezoo/edge/controllers/ZoolController.java
+++ src/main/java/com/dezoo/edge/controllers/ZoolController.java
@@ -1,13 +1,15 @@
 package com.dezoo.edge.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("animals")
-public class AnimalController {
+public class ZoolController {
 	@Autowired
 	private RestTemplate restTemplate;
+
+	@Value("${personnelservice.baseurl}")
+	private String personnelServiceBaseUrl;
 }
