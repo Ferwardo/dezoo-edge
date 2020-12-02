c18f7990db68ce2749ad8228d4ef8d0cee6b0a33
Ferwardo <r0741339@student.thomasmore.be>
2020-12-02 14:01:41 +0100

    Added post function for personnelMembers

--- src/main/java/com/dezoo/edge/controllers/ZooController.java
+++ src/main/java/com/dezoo/edge/controllers/ZooController.java
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -30,6 +27,13 @@ public class ZooController {
 	@Value("${residenceService.baseurl}")
 	private String residenceServiceBaseUrl;
 
+	//Personnel functions
+	//TODO add function to get personnel with the residence they work in
+	/**
+	 * Gets all personnel from the personnel microservices
+	 * @return            a list of PersonnelMember object representing the personnel members of the zoo
+	 * @author            Ferwardo (Ferre Snyers)
+	 */
 	@GetMapping("/personeel")
 	public List<PersonnelMember> getPersonnelmembers(){
 		ResponseEntity<List<PersonnelMember>> responseEntityPersonnelMembers =
@@ -39,11 +43,20 @@ public class ZooController {
 		return responseEntityPersonnelMembers.getBody();
 	}
 
+	/**
+	 * gets a personnel member from the personnel microservices using a personnelId
+	 * @param personnelID The personnelId of the personnelMember that needs to be returned.
+	 * @return            One PersonnelMember corresponding to the given personnelId
+	 * @author            Ferwardo (Ferre Snyers)
+	 */
 	@GetMapping("/personeel/{personnelID}")
 	public PersonnelMember getPersonnelMemberByPersonnelId(@PathVariable String personnelID){
-		PersonnelMember personnelMember =
-				restTemplate.getForObject("http://"+personnelServiceBaseUrl+"/personnel/{personnelID}",
-				PersonnelMember.class, personnelID);
-		return personnelMember;
+		return restTemplate.getForObject("http://"+personnelServiceBaseUrl+"/personnel/{personnelID}",
+		PersonnelMember.class, personnelID);
+	}
+
+	@PostMapping("/personeel")
+	public PersonnelMember addPersonnelMember(@RequestBody PersonnelMember personnelMember){
+		return restTemplate.postForObject("http://"+personnelServiceBaseUrl+"/personnel",personnelMember,PersonnelMember.class);
 	}
 }
