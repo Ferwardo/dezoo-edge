299f14530909f135b08135eea5c1c7e8a63ee971
Ferwardo <r0741339@student.thomasmore.be>
2021-01-09 14:57:20 +0100

    Added all unit tests for residences

--- src/test/java/com/dezoo/edge/residenceTests/ResidenceUnitTests.java
+++ src/test/java/com/dezoo/edge/residenceTests/ResidenceUnitTests.java
@@ -25,9 +25,8 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -52,11 +51,11 @@ public class ResidenceUnitTests {
     @BeforeEach
     public void initialise() {
         mockServer = MockRestServiceServer.createServer(restTemplate);
-        residence1 = new Residence("leeuwen001", "fs161100","l001", "Pride Rock", 4, "1995", false );
+        residence1 = new Residence("leeuwen001", "fs161100", "l001", "Pride Rock", 4, "1995", false);
 
-        residenceList = new ArrayList<Residence>();
-        residenceList.add(new Residence("1","konijnen001", "cn170999","r001", "De Konijnenpijp", 15, "2010", false ));
-        residenceList.add(new Residence("eekhoorns001", "cn170999","s001", "De Grote Boom", 8, "2020", false ));
+        residenceList = new ArrayList<>();
+        residenceList.add(new Residence("1", "konijnen001", "cn170999", "r001", "De Konijnenpijp", 15, "2010", false));
+        residenceList.add(new Residence("eekhoorns001", "cn170999", "s001", "De Grote Boom", 8, "2020", false));
     }
 
     @Test
@@ -111,6 +110,7 @@ public class ResidenceUnitTests {
                 .andExpect(jsonPath("[1].bouwJaar", is("2020")))
                 .andExpect(jsonPath("[1].nocturnal", is(false)));
     }
+
     @Test
     public void whenGetResidenceByAnimalId_thenReturnResidenceJson() throws Exception {
         residenceList.remove(1);
@@ -135,4 +135,77 @@ public class ResidenceUnitTests {
                 .andExpect(jsonPath("[0].nocturnal", is(false)));
     }
 
+    @Test
+    public void whenAddResidence_thenReturnResidenceJson() throws Exception {
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://" + residenceServiceBaseUrl + "/verblijven")))
+                .andExpect(method(HttpMethod.POST))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(residence1))
+                );
+
+        mockMvc.perform(post("/verblijven")
+                .content(mapper.writeValueAsString(residence1))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.verblijfID", is("leeuwen001")))
+                .andExpect(jsonPath("$.name", is("Pride Rock")))
+                .andExpect(jsonPath("$.personeelID", is("fs161100")))
+                .andExpect(jsonPath("$.dierID", is("l001")))
+                .andExpect(jsonPath("$.maxDieren", is(4)))
+                .andExpect(jsonPath("$.bouwJaar", is("1995")))
+                .andExpect(jsonPath("$.nocturnal", is(false)));
+    }
+
+    @Test
+    public void whenUpdateResidenc_thenReturnResidenceJson() throws Exception {
+        Residence updatedResidence = new Residence("leeuwen001", "fs161100", "l001", "Pride Rock", 4, "1995", false);
+
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://" + residenceServiceBaseUrl + "/verblijven/")))
+                .andExpect(method(HttpMethod.PUT))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(updatedResidence))
+                );
+
+        mockMvc.perform(put("/verblijven")
+                .content(mapper.writeValueAsString(updatedResidence))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.verblijfID", is("leeuwen001")))
+                .andExpect(jsonPath("$.name", is("Pride Rock")))
+                .andExpect(jsonPath("$.personeelID", is("fs161100")))
+                .andExpect(jsonPath("$.dierID", is("l001")))
+                .andExpect(jsonPath("$.maxDieren", is(4)))
+                .andExpect(jsonPath("$.bouwJaar", is("1995")))
+                .andExpect(jsonPath("$.nocturnal", is(false)));
+    }
+
+    @Test
+    public void whenDeleteResidence_thenStatusOk() throws Exception{
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://" + residenceServiceBaseUrl + "/verblijven/leeuwen001")))
+                .andExpect(method(HttpMethod.DELETE))
+                .andRespond(withStatus(HttpStatus.OK));
+
+        mockMvc.perform(delete("/verblijven/{verblijfID}", "leeuwen001")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void whenDeleteAnimal_thenStatusInternalServerError() throws Exception{
+        mockServer.expect(ExpectedCount.once(),
+                requestTo(new URI("http://" + residenceServiceBaseUrl + "/verblijven/leeuwen002")))
+                .andExpect(method(HttpMethod.DELETE))
+                .andRespond(withStatus(HttpStatus.INTERNAL_SERVER_ERROR));
+
+        mockMvc.perform(delete("/verblijven/{verblijfID}", "leeuwen002")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is5xxServerError());
+    }
 }
